342c5903a68729779ea3eb0981ff13c902411a4b
Distortions81 <carlotto81@gmail.com>
2024-02-29 13:03:12 -0700

    auto elipsis

--- draw.go
+++ draw.go
@@ -14,6 +14,8 @@ const pixelsPerLine = 36
 const pad = 8
 const namePixels = 170
 const maxMsgLen = 300
+
+const maxNameLen = 15
 const chatLife = time.Second * 30
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -38,8 +40,32 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		if time.Since(chatHistory[x].time) > chatLife {
 			continue
 		}
-		text.Draw(screen, sclean.TruncateString(chatHistory[x].sender, 15), mplusNormalFont, pad, justify+(z*pixelsPerLine)-pad, chatHistory[x].color)
-		text.Draw(screen, sclean.TruncateStringEllipsis(": "+chatHistory[x].message, maxMsgLen), mplusNormalFont, pad+namePixels, justify+(z*pixelsPerLine)-pad, color.White)
+		name := sclean.TruncateString(chatHistory[x].sender, maxNameLen)
+		namePix := text.BoundString(mplusNormalFont, name)
+
+		maxAttempt := 1000
+		count := 0
+		lineLen := maxMsgLen
+		for {
+			count++
+
+			msgPix := text.BoundString(mplusNormalFont, sclean.TruncateStringEllipsis(": "+chatHistory[x].message, lineLen))
+			if msgPix.Max.X > (ScreenWidth - namePixels - pad) {
+				lineLen--
+			} else {
+				break
+			}
+
+			if count > maxAttempt {
+				lineLen = maxMsgLen
+				break
+			}
+		}
+
+		msg := sclean.TruncateStringEllipsis(": "+chatHistory[x].message, lineLen)
+
+		text.Draw(screen, name, mplusNormalFont, (namePixels - namePix.Dx()), justify+(z*pixelsPerLine)-pad, chatHistory[x].color)
+		text.Draw(screen, msg, mplusNormalFont, pad+namePixels, justify+(z*pixelsPerLine)-pad, color.White)
 	}
 }
 
