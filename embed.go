ba6a259d75d9c985c72f10626c34e94656421897
Distortions81 <carlotto81@gmail.com>
2024-03-15 21:57:03 -0600

    add bg

--- embed.go
+++ embed.go
@@ -3,12 +3,24 @@ package main
 import (
 	"embed"
 	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 var (
 	//go:embed data
-	f embed.FS
+	f     embed.FS
+	bgimg *ebiten.Image
 )
+
+func init() {
+	var err error
+	bgimg, _, err = ebitenutil.NewImageFromFile("data/maps/main.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func getFont(name string) []byte {
 	data, err := f.ReadFile("data/fonts/" + name)
--- map.go
+++ map.go
@@ -11,12 +11,14 @@ import (
 )
 
 const (
-	size         = 22
-	mag          = size + 1
-	boardSize    = 25
-	itemOffset   = 2
-	offsetPixels = size * itemOffset
-	boardPixels  = ((boardSize) * mag)
+	size        = 32
+	mag         = size
+	boardSize   = 18
+	offX        = 5
+	offY        = 1
+	offPixX     = size * offX
+	offPixY     = size * offY
+	boardPixels = ((boardSize) * mag)
 )
 
 type objectData struct {
@@ -40,31 +42,28 @@ func drawGameBoard(screen *ebiten.Image) {
 	if board.bgDirty {
 		board.bgCache.Clear()
 
+		board.bgCache.DrawImage(bgimg, nil)
+
 		//Draw left side bg red
 		for x := 0; x < boardSize; x++ {
 			for y := 0; y < boardSize; y++ {
 				var tColor color.Color
-				if x == 0 {
-					tColor = ColorLightGray
-					if (x+y)%2 == 0 {
-						tColor = ColorGray
-					}
-				} else {
-					tColor = ColorDarkGreen
-					if (x+y)%2 == 0 {
-						tColor = ColorGreen
-					}
+
+				tColor = ColorDarkGreen
+				if (x+y)%2 == 0 {
+					tColor = ColorGreen
 				}
-				vector.DrawFilledRect(board.bgCache, float32(mag*x)+offsetPixels, float32(mag*y)+offsetPixels, size, size, tColor, true)
+
+				vector.DrawFilledRect(board.bgCache, float32(mag*x)+offPixX, float32(mag*y)+offPixY, size, size, tColor, true)
 
 				//Draw coords
 				if y == 0 {
 					buf := fmt.Sprintf("%v", x+1)
-					text.Draw(board.bgCache, buf, monoFontSmall, (mag*x)+offsetPixels+5, (mag*y)+offsetPixels-3, color.White)
+					text.Draw(board.bgCache, buf, monoFontSmall, (mag*x)+offPixX+5, (mag*y)+offPixY-3, color.White)
 				}
 				if x == 0 {
 					buf := fmt.Sprintf("%2v", y+1)
-					text.Draw(board.bgCache, buf, monoFontSmall, (mag*x)+(offsetPixels/2)+5, (mag*y)+offsetPixels+15, color.White)
+					text.Draw(board.bgCache, buf, monoFontSmall, (mag*x)+(offPixX/2)+5, (mag*y)+offPixY+15, color.White)
 				}
 
 				//XY Labels
@@ -73,30 +72,6 @@ func drawGameBoard(screen *ebiten.Image) {
 			}
 		}
 
-		//Draw right side bg blue
-		for x := 0; x < boardSize; x++ {
-			for y := 0; y < boardSize; y++ {
-				var tColor color.Color
-
-				//Sand, water
-				if x < 20 {
-					tColor = ColorLightBrown
-					if (x+y)%2 == 0 {
-						tColor = ColorBrown
-					}
-				} else {
-					tColor = ColorAqua
-					if (x+y)%2 == 0 {
-						tColor = ColorDarkAqua
-					}
-				}
-				vector.DrawFilledRect(board.bgCache, float32(mag*x)+(offsetPixels)+boardPixels, float32(mag*y)+offsetPixels, size, size, tColor, true)
-			}
-		}
-
-		//Draw board lables
-		text.Draw(board.bgCache, "Audience", monoFont, boardPixels/2, boardPixels+65, color.White)
-		text.Draw(board.bgCache, "Computer", monoFont, boardPixels+boardPixels/2, boardPixels+65, color.White)
 		board.bgDirty = false
 	}
 	screen.DrawImage(board.bgCache, nil)
@@ -104,7 +79,7 @@ func drawGameBoard(screen *ebiten.Image) {
 	//Draw towers
 	board.lock.Lock()
 	for _, item := range board.bmap {
-		vector.DrawFilledCircle(screen, float32((item.Pos.X+itemOffset)*mag)-(size/1.5), float32((item.Pos.Y+itemOffset)*mag)-(size/1.5), size/2, color.White, true)
+		vector.DrawFilledCircle(screen, float32((item.Pos.X+offX)*mag)-(size/1.5), float32((item.Pos.Y+offY)*mag)-(size/1.5), size/2, color.White, true)
 	}
 	board.lock.Unlock()
 
