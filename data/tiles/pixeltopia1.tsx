3cb771c09bd0816678b7ed58ba8b81568c9c3117
Distortions81 <carlotto81@gmail.com>
2024-03-15 20:58:15 -0600

    new tiles

---
+++ data/tiles/pixeltopia1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pixeltopia1" tilewidth="32" tileheight="32" tilecount="1800" columns="50">
+ <image source="32x32.png" width="1600" height="1152"/>
+</tileset>
